9f33327028161d4d5873003294f0950465473a75
Subin S K <subinsk284@gmail.com>
2023-01-20 14:20:22 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#13005A',
+                'primary-light': '#00337C',
+            },
+        },
+    },
+    plugins: [],
+}
