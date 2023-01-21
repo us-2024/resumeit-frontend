34d92e793284cea6152d9a57334dc7b6e1070f57
Subin S K <subinsk284@gmail.com>
2023-01-21 14:28:25 +0530

    done

--- src/services/sendMail.ts
+++ src/services/sendMail.ts
@@ -1,18 +1,29 @@
 import { useAuth } from '@/contexts/auth'
 import { backendApi } from '@/utils/backendApi'
+import { toast } from 'react-hot-toast'
 
-export const sendMail = (
+export const sendMail = async (
     user_id: string,
     reciever_addresses: any,
     subject: string,
-    body: string
+    body: string,
+    file: any
 ) => {
-    var formData = new FormData()
+    try {
+        var formData = new FormData()
 
-    formData.append('user_id', user_id)
-    formData.append('reciever_addresses', reciever_addresses.join(','))
-    formData.append('subject', subject)
-    formData.append('body', body)
-
-    backendApi.post('/sendemail/')
+        formData.append('user_id', user_id)
+        formData.append('reciever_addresses', reciever_addresses.join(','))
+        formData.append('subject', subject)
+        formData.append('body', body)
+        formData.append('file', file)
+        const { data } = await backendApi.post('/sendemail/', {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+        })
+        toast.success(data.Status)
+    } catch (err: any) {
+        toast.error(err.message)
+    }
 }
